86507d1b69daad0d29fb03fe24b04a60955bf504
Christian Joseph Tobin <40743167+christiantobin@users.noreply.github.com>
2024-07-18 16:21:05 -0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,31 +13,3 @@ require("tokyonight").setup({
 })
 
 vim.cmd("colorscheme tokyonight")
-
--- Set highlight groups for transparency
-vim.cmd([[
-  highlight Normal guibg=none ctermbg=none
-  highlight NonText guibg=none ctermbg=none
-  highlight LineNr guibg=none ctermbg=none
-  highlight Folded guibg=none ctermbg=none
-  highlight EndOfBuffer guibg=none ctermbg=none
-  highlight SignColumn guibg=none ctermbg=none
-  highlight StatusLineNC guibg=none ctermbg=none
-  highlight VertSplit guibg=none ctermbg=none
-  highlight TabLine guibg=none ctermbg=none
-  highlight TabLineFill guibg=none ctermbg=none
-  highlight TabLineSel guibg=none ctermbg=none
-
-  " Additional highlight groups for Neo-tree
-  highlight NeoTreeNormal guibg=none ctermbg=none
-  highlight NeoTreeEndOfBuffer guibg=none ctermbg=none
-
-  " Highlight groups for which-key
-  highlight WhichKey guibg=none ctermbg=none
-  highlight WhichKeyFloat guibg=none ctermbg=none
-  highlight WhichKeyGroup guibg=none ctermbg=none
-  highlight WhichKeyDesc guibg=none ctermbg=none
-  highlight WhichKeySeperator guibg=none ctermbg=none
-  highlight WhichKeyValue guibg=none ctermbg=none
-]])
-
