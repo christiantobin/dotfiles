d812e5a85089334aeafad7f1ea5b9201ff1377aa
christiantobin <christian@wirelesseco.com>
2025-04-02 16:41:12 -0700

    Add plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,4 @@
 require("config.lazy")
 
 vim.cmd("colorscheme tokyonight")
+vim.opt.clipboard = "unnamedplus"
---
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -0,0 +1,10 @@
+return {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+        "mhartington/formatter.nvim",
+        "nvim-lua/plenary.nvim",
+        "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+}
